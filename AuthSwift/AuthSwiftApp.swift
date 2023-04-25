6e6ae9d3ca6fce412a5b4c9d78aa57d97996bb8d
Vcky4 <okon.victor.u@.com>
2023-04-25 23:08:14 +0100

    Initial Commit

---
+++ AuthSwift/AuthSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthSwiftApp.swift
+//  AuthSwift
+//
+//  Created by mac on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
